e2c1a52467a65ed643c4ee23400e7287f8c95844
Peter Pham <peterpham86@gmail.com>
2019-09-11 07:38:59 +0000

    almost there..

--- main.cpp
+++ main.cpp
@@ -59,8 +59,6 @@ int main(){
     outb(0x07,EFIR);//Set Device number register
     outb(0x0B,EFDR); //Select Device "B"
 
-
-
     //get base addresss of hardware monitor device B
     // from CR[60h] & CR[61h]
     printf("Reading hardware monitor base address\n");
@@ -71,6 +69,42 @@ int main(){
     base_address = t1 << 8 | t2;
     printf("base_address: %x\n",base_address);
 
+    /*printf("Reading raw CPU fan value\n");
+    //3 means it's 31.3 khz output frequency
+    int cpu_fan_value_offset = 0x03;
+    int cpu_fan_value;
+    outb(index+cpu_fan_value_offset,EFIR);
+    cpu_fan_value = inb(EFDR);
+    printf("%x\n",cpu_fan_value);
+
+    printf("Reading CPU fan clock divider signal\n");
+    //3 means it's 31.3 khz output frequency
+    int cpu_fan_divider_offset = 0x02;
+    outb(index+cpu_fan_divider_offset,EFIR);
+    int cpu_fan_divider = inb(EFDR);
+    printf("%x\n",cpu_fan_divider);*/
+
+    //Sensor base address
+    int index = base_address + 5;
+    int data = base_address + 6;
+
+    /*if (ioperm(index, 2, 1))
+       {perror("ioperm error");return 0;}
+
+    printf("Select bank 4 to read CPU RPM\n");
+    int bank_set_offset = 0x4e;
+    outb(0x4e ,index);
+    outb(0x04,data);
+  
+    printf("Reading CPU fan RPM\n");
+    int cpu_fan_value_offset_msb = 0xC2;
+    int cpu_fan_value_offset_lsb = 0xC3;
+    outb(cpu_fan_value_offset_msb, index);
+    t1 = inb(data);
+    outb(cpu_fan_value_offset_lsb, index);
+    t1 = inb(data);
+    printf("%d\n",t1 >> 8 | t2);  
+    */
 
     //exit extended function mode
     printf("Exit extended mode\n");
